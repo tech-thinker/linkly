7bf241eee70f2b326bbe8504edc005828db6ae8f
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2022-01-31 19:44:22 +0530

    fix: add env PORT support

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"embed"
 	"log"
+	"os"
 
 	"github.com/tech-thinker/linkly/api/routes"
 
@@ -13,11 +14,19 @@ import (
 var viewsFs embed.FS
 
 func main() {
+	// Get port from env
+	port := ":3000"
+	_, present := os.LookupEnv("PORT")
+	if present {
+		port = ":" + os.Getenv("PORT")
+
+	}
+
 	// Set the router as the default one shipped with Gin
 	server := gin.Default()
 	// Initialize the routes
 	routes.ViewsFs = viewsFs
 	routes.InitRoutes(server)
 	// Start and run the server
-	log.Fatal(server.Run(":8080"))
+	log.Fatal(server.Run(port))
 }
