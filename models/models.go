1d1b3cc791e40c911d58a88f5245bc229f53134d
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2022-07-19 17:32:12 +0530

    fix: update models struct

--- models/models.go
+++ models/models.go
@@ -1,15 +1,22 @@
 package models
 
+import (
+	"database/sql"
+	"time"
+)
+
 // URL struct information about URL
 type URL struct {
-	ID        uint64 `json:"id" gorm:"primary_key"`
-	URL       string `json:"url"`
-	ShortURL  string `json:"short_url"`
-	Expires   string `json:"expires"`
-	Visits    int    `json:"visits"`
-	UserID    string `json:"user_id"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
+	ID        uint64       `json:"id,omitempty" gorm:"primary_key,autoIncrement,not null"`
+	URL       string       `json:"url,omitempty"`
+	ShortURL  string       `json:"short_url,omitempty"`
+	Expires   string       `json:"expires,omitempty"`
+	Visits    int          `json:"visits,omitempty"`
+	UserID    string       `json:"user_id,omitempty"`
+	IP        string       `json:"ip,omitempty"`
+	CreatedAt time.Time    `json:"created_at,omitempty" gorm:"not null"`
+	UpdatedAt time.Time    `json:"updated_at,omitempty" gorm:"not null"`
+	DeletedAt sql.NullTime `json:"deleted_at,omitempty" gorm:"index,not null"`
 }
 
 // Response struct
@@ -18,21 +25,19 @@ type Response struct {
 	Data    interface{} `json:"urls"`
 }
 
-// // Input get input from request
-// type Input struct {
-// 	// gorm.Model
-// 	URL      string `json:"url"`
-// 	ShortURL string `json:"short_url"`
-// 	Expires  string `json:"expires"`
-// }
-
 // User struct
 type User struct {
-	ID        uint64 `json:"id" gorm:"primary_key"`
-	UserID    string `json:"user_id" gorm:"default:uuid_generate_v3()"`
-	Email     string `json:"email"`
-	CreatedAt string `json:"created_at" gorm:"autoCreateTime"`
-	UpdatedAt string `json:"updated_at" gorm:"autoUpdateTime"`
+	ID        uint64       `json:"id,omitempty" gorm:"primary_key,autoIncrement,not null"`
+	UserID    string       `json:"user_id,omitempty" gorm:"default:uuid_generate_v3()"`
+	Username  string       `json:"username"  gorm:"unique,not null"`
+	Email     string       `json:"email,omitempty"`
+	Password  string       `json:"password" gorm:"not null"`
+	IPs       []string     `json:"ips,omitempty"`
+	Role      string       `json:"role" gorm:"not null"`
+	Level     int          `json:"level" gorm:"not null"`
+	CreatedAt time.Time    `json:"created_at,omitempty" gorm:"not null"`
+	UpdatedAt time.Time    `json:"updated_at,omitempty" gorm:"not null"`
+	DeletedAt sql.NullTime `json:"deleted_at,omitempty" gorm:"index,not null"`
 }
 
 // QRCode struct
